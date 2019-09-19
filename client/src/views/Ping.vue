49f9a04e1de04aee689971e315b757cc41f68050
jparenas <juan.p.arenas1@gmail.com>
2019-09-19 15:48:20 -0400

    Added WebSocket support for the ping example

--- client/src/views/Ping.vue
+++ client/src/views/Ping.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="ping">
-    <input v-model="pingMessage" placeholder="Message to ping" @keyup.enter="send" />
-    <button @click.stop="send">Send</button>
+    <input v-model="pingMessage" placeholder="Message to ping" @keyup.enter="sendHTTP" />
+    <button @click.stop="sendHTTP">Send with HTTP</button>
+    <button @click.stop="sendSocket">Send with WebSockets</button>
+    <br />
+    <span>Ping through HTTP</span>
     <br />
     <b>
       <span>Ping responses:</span>
     </b>
     <br />
-    <div v-for="(response, i) in responses" :key="i">
+    <div v-for="(response, i) in httpResponses" :key="i + 'http'">
+      <span>Message: {{response.message}} Time Taken: {{response.time}}ms</span>
+      <br />
+    </div>
+    <span>Ping through WebSockets, status: {{status}}</span>
+    <br />
+    <b>
+      <span>Ping responses:</span>
+    </b>
+    <br />
+    <div v-for="(response, i) in socketResponses" :key="i + 'socket'">
       <span>Message: {{response.message}} Time Taken: {{response.time}}ms</span>
       <br />
     </div>
@@ -16,15 +29,49 @@
 
 <script>
 import axios from "axios";
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
 
 export default {
   name: "ping",
   data: () => ({
     pingMessage: "",
-    responses: []
+    httpResponses: [],
+    socketResponses: [],
+    connected: false
   }),
+  computed: {
+    status() {
+      if (this.connected) {
+        return "Connected";
+      } else {
+        return "Not Connected";
+      }
+    }
+  },
+  created() {
+    this.socket = new SockJS(`${process.env.VUE_APP_API_HOST}/socket/ping`);
+    this.stompClient = Stomp.over(this.socket);
+    this.stompClient.connect(
+      {},
+      frame => {
+        this.connected = true;
+        this.stompClient.subscribe("/user/topic/ping", tick => {
+          const end = performance.now();
+          this.socketResponses.push({
+            message: tick.body,
+            time: (end - this.socketStart).toFixed(2)
+          });
+        });
+      },
+      error => {
+        console.log(error);
+        this.connected = false;
+      }
+    );
+  },
   methods: {
-    async send(event) {
+    async sendHTTP(event) {
       const start = performance.now();
       // Send the request to the server and wait for the response.
       const result = await axios.get(`${process.env.VUE_APP_API_HOST}/ping`, {
@@ -33,10 +80,17 @@ export default {
         }
       });
       const end = performance.now();
-      this.responses.push({
+      this.httpResponses.push({
         message: result.data,
         time: (end - start).toFixed(2)
       });
+      this.pingMessage = "";
+    },
+    sendSocket(event) {
+      if (this.stompClient && this.stompClient.connected) {
+        this.socketStart = performance.now();
+        this.stompClient.send("/app/ping", this.pingMessage, {});
+      }
       this.pingMessage = "";
     }
   }
