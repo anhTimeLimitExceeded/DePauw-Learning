108a3efac941ed278c8f911fa79de61c33a81406
jparenas <juan.p.arenas1@gmail.com>
2019-09-24 09:50:23 -0400

    Simplified classes and added more tests

---
+++ server/src/test/java/edu/depauw/itap/compiler/CompilerControllerSocketTest.java
@@ -0,0 +1,107 @@
+package edu.depauw.itap.compiler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CompilerControllerSocketTest {
+  @LocalServerPort
+  int randomPort;
+
+  private StompSession stompSession;
+
+  private CompletableFuture<CompilerResponse> completableFuture;
+
+  @Before
+  public void setup() throws InterruptedException, ExecutionException, TimeoutException {
+    String hostUrl = "ws://localhost:" + randomPort + "/socket";
+
+    WebSocketStompClient stompClient =
+        new WebSocketStompClient(new SockJsClient(createTransportClient()));
+    stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+    stompSession = stompClient.connect(hostUrl, new StompSessionHandlerAdapter() {
+    }).get(1, TimeUnit.SECONDS);
+
+    completableFuture = new CompletableFuture<>();
+  }
+
+  @Test
+  public void testCompileEndpoint() throws Exception {
+    String source = "public class Test {\n" + "public static void main(String[] args) {\n"
+        + "    System.out.println(\"Hello World!\");\n" + " }\n" + "}\n";
+
+    stompSession.subscribe("/user/topic/compile", new CompileStompFrameHandler());
+    stompSession.send("/app/compile",
+        new CompilerSources().setSources(Collections.singletonList(source)));
+
+    CompilerResponse compilerResults = completableFuture.get(5, TimeUnit.SECONDS);
+
+    assertThat(compilerResults).isNotNull();
+    assertThat(compilerResults.getResults()).isEmpty();
+  }
+
+  @Test
+  public void testCompileEndpointBadCode() throws Exception {
+    String source = "public class Test {\n" + "public static void main(String[] args) {\n"
+        + "    System.out.printaln(\"Hello World!\");\n" + " }\n" + "}\n";
+
+    stompSession.subscribe("/user/topic/compile", new CompileStompFrameHandler());
+    stompSession.send("/app/compile",
+        new CompilerSources().setSources(Collections.singletonList(source)));
+
+    CompilerResponse compilerResults = completableFuture.get(5, TimeUnit.SECONDS);
+
+    assertThat(compilerResults).isNotNull();
+    assertThat(compilerResults.getResults()).isNotEmpty();
+    assertThat(compilerResults.getResults().get(0).getClassName()).isEqualTo("Test");
+    assertThat(compilerResults.getResults().get(0).getMessage()).isNotEmpty();
+    assertThat(compilerResults.getResults().get(0).getSeverity()).isEqualTo("ERROR");
+    assertThat(compilerResults.getResults().get(0).getStartLineNumber()).isGreaterThan(0);
+    assertThat(compilerResults.getResults().get(0).getStartColumnNumber()).isGreaterThan(0);
+    assertThat(compilerResults.getResults().get(0).getEndLineNumber()).isGreaterThan(0);
+    assertThat(compilerResults.getResults().get(0).getEndColumnNumber()).isGreaterThan(0);
+  }
+
+  private class CompileStompFrameHandler implements StompFrameHandler {
+    @Override
+    public Type getPayloadType(StompHeaders stompHeaders) {
+      return CompilerResponse.class;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders stompHeaders, Object o) {
+      completableFuture.complete((CompilerResponse) o);
+    }
+  }
+
+  private List<Transport> createTransportClient() {
+    List<Transport> transports = new ArrayList<>(1);
+    transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+    return transports;
+  }
+}
--- server/src/test/java/edu/depauw/itap/compiler/CompilerServiceTest.java
+++ server/src/test/java/edu/depauw/itap/compiler/CompilerServiceTest.java
@@ -1,53 +1,84 @@
 package edu.depauw.itap.compiler;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
+import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
 @RunWith(JUnit4.class)
 public class CompilerServiceTest {
-  @InjectMocks
   CompilerService compilerService;
 
-  @Spy
-  CompilerRunnable compilerRunnable;
+  private static String VALID_SOURCE =
+      "public class Test {\n" + "public static void main(String[] args) {\n"
+          + "    System.out.println(\"Hello World!\");\n" + " }\n" + "}\n";
+
+  private static String INVALID_SOURCE =
+      "public class Test {\n" + "public static void main(String[] args) {\n"
+          + "    INVALID(\"Hello World!\");\n" + " }\n" + "}\n";
+
+  private static String VALID_SOURCE_WITH_PACKAGE = "package test.moretest;\n" + VALID_SOURCE;
 
   @Before
-  public void initMocks() {
-    MockitoAnnotations.initMocks(this);
+  public void setup() {
+    compilerService = new CompilerService();
   }
 
   @Test
-  public void testCompilingSource() {
+  public void testCompilingWithoutSaving() {
+    List<String> sources = Collections.singletonList(VALID_SOURCE);
+
+    List<CompilerResult> results = compilerService.compileWithoutSaving("test", sources);
+
+    assertThat(results).isNotNull();
+    assertThat(results).isEmpty();
+  }
+
+  @Test
+  public void testCompilingWithoutSavingDoesNotSaveFiles() {
+    List<String> sources = Collections.singletonList(VALID_SOURCE);
+
+    compilerService.compileWithoutSaving("test", sources);
+
+    File tempRoot = CompilerService.getDirectoryPath("test");
+    assertThat(tempRoot).doesNotExist();
+  }
+
+  @Test
+  public void testCompiling() {
+    List<String> sources = Collections.singletonList(VALID_SOURCE);
+
+    List<CompilerResult> results = compilerService.compile("test", sources);
+
+    assertThat(results).isNotNull();
+    assertThat(results).isEmpty();
+  }
+
+  @Test
+  public void testCompilingSaveFiles() {
     String source = "public class Test {\n" + "public static void main(String[] args) {\n"
         + "    System.out.println(\"Hello World!\");\n" + " }\n" + "}\n";
 
     List<String> sources = Collections.singletonList(source);
 
-    List<CompilerResult> results = compilerService.compile("test", sources);
+    compilerService.compile("test", sources);
 
-    verify(compilerRunnable).compileWithoutSaving("test", sources);
-    assertThat(results).isEmpty();
+    File tempRoot = CompilerService.getDirectoryPath("test");
+    assertThat(tempRoot).exists();
   }
 
   @Test
-  public void testCompilingBadSource() {
-    String source = "public class Test {\n" + "public statics void main(String[] args) {\n"
-        + "    System.out.println(\"Hello World!\");\n" + " }\n" + "}\n";
-
-    List<String> sources = Collections.singletonList(source);
+  public void testCompilingInvalidSource() {
+    List<String> sources = Collections.singletonList(INVALID_SOURCE);
 
     List<CompilerResult> results = compilerService.compile("test", sources);
 
-    verify(compilerRunnable).compileWithoutSaving("test", sources);
+    assertThat(results).isNotNull();
     assertThat(results).isNotEmpty();
     assertThat(results.get(0).getClassName()).isEqualTo("Test");
     assertThat(results.get(0).getMessage()).isNotEmpty();
@@ -56,5 +87,37 @@ public class CompilerServiceTest {
     assertThat(results.get(0).getStartColumnNumber()).isGreaterThan(0);
     assertThat(results.get(0).getEndLineNumber()).isGreaterThan(0);
     assertThat(results.get(0).getEndColumnNumber()).isGreaterThan(0);
+  }
+
+  @Test
+  public void testGetClassName() {
+    String className = CompilerService.getFullyQualifiedClassName(VALID_SOURCE);
+
+    assertThat(className).isEqualTo("Test");
+  }
+
+  @Test
+  public void testGetClassNameWithPackage() {
+    String className = CompilerService.getFullyQualifiedClassName(VALID_SOURCE_WITH_PACKAGE);
+
+    assertThat(className).isEqualTo("test.moretest.Test");
+  }
+
+  @After
+  public void teardown() {
+    File tempRoot = CompilerService.getDirectoryPath("test");
+    if (tempRoot.exists()) {
+      deleteDirectory(tempRoot);
+    }
+  }
+
+  private static boolean deleteDirectory(File directoryToBeDeleted) {
+    File[] allContents = directoryToBeDeleted.listFiles();
+    if (allContents != null) {
+      for (File file : allContents) {
+        deleteDirectory(file);
+      }
+    }
+    return directoryToBeDeleted.delete();
   }
 }
